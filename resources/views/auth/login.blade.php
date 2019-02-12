170e0664ef8ce48a45bd866337c6406b45d96192
Indra Hasan <verbosefall@gmail.com>
2019-02-12 19:27:16 +0700

    update template to adminLTE

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,107 @@
-@extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Login &mdash; Stisla</title>
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="{{ asset('stisla') }}/modules/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="{{ asset('stisla') }}/modules/fontawesome/css/all.min.css">
+
+  <!-- CSS Libraries -->
+  <link rel="stylesheet" href="{{ asset('stisla') }}/modules/bootstrap-social/bootstrap-social.css">
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="{{ asset('stisla') }}/css/style.css">
+  <link rel="stylesheet" href="{{ asset('stisla') }}/css/components.css">
+</head>
+
+<body>
+  <div id="app">
+    <section class="section">
+      <div class="container mt-5">
+        <div class="row">
+          <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+            <div class="login-brand">
+              <img src="{{ asset('stisla') }}/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
+            </div>
+
+            <div class="card card-primary">
+              <div class="card-header"><h4>Login</h4></div>
+
+              <div class="card-body">
+                <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
                         {{ csrf_field() }}
+                  
+                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        <label for="email">Email</label>
+                        <input id="email" type="email" class="form-control" name="email" tabindex="1" >
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                {{ $errors->first('email') }}
+                            </span>
+                        @endif                    
+                    </div>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
+                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <div class="d-block">
+                            <label for="password" class="control-label">Password</label>
+                            <div class="float-right">
+                                <a href="{{ route('password.request') }}" class="text-small">
+                                Forgot Password?
                                 </a>
                             </div>
                         </div>
-                    </form>
-                </div>
+                        <input id="password" type="password" class="form-control" name="password" tabindex="2" >
+                        @if ($errors->has('password'))
+                            <span class="help-block">
+                                {{ $errors->first('password') }}
+                            </span>
+                        @endif
+                    </div>
+
+                  {{-- <div class="form-group">
+                    <div class="custom-control custom-checkbox">
+                      <input type="checkbox" name="remember" class="custom-control-input" tabindex="3" id="remember-me">
+                      <label class="custom-control-label" for="remember-me">Remember Me</label>
+                    </div>
+                  </div> --}}
+
+                  <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                      Login
+                    </button>
+                  </div>
+                </form>
+               
+              </div>
             </div>
+            <div class="simple-footer">
+              Copyright &copy; Stisla 2018
+            </div>
+          </div>
         </div>
-    </div>
-</div>
-@endsection
+      </div>
+    </section>
+  </div>
+
+  <!-- General JS Scripts -->
+  <script src="{{ asset('stisla') }}/modules/jquery.min.js"></script>
+  <script src="{{ asset('stisla') }}/modules/popper.js"></script>
+  <script src="{{ asset('stisla') }}/modules/tooltip.js"></script>
+  <script src="{{ asset('stisla') }}/modules/bootstrap/js/bootstrap.min.js"></script>
+  <script src="{{ asset('stisla') }}/modules/nicescroll/jquery.nicescroll.min.js"></script>
+  <script src="{{ asset('stisla') }}/modules/moment.min.js"></script>
+  <script src="{{ asset('stisla') }}/js/stisla.js"></script>
+  
+  <!-- JS Libraies -->
+
+  <!-- Page Specific JS File -->
+  
+  <!-- Template JS File -->
+  <script src="{{ asset('stisla') }}/js/scripts.js"></script>
+  <script src="{{ asset('stisla') }}/js/custom.js"></script>
+</body>
+</html>
